fa0d262b181fb8fb0f502e83f591045c235d0513
dmytro.shmidt <dmytro.shmidt@cgi.com>
2023-05-20 11:04:28 +0200

    validations to Customer entity

--- src/main/java/com/test/quotation/model/dto/CustomerDto.java
+++ src/main/java/com/test/quotation/model/dto/CustomerDto.java
@@ -1,30 +1,17 @@
 package com.test.quotation.model.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import jakarta.annotation.Nullable;
-import jakarta.persistence.Temporal;
-import jakarta.persistence.TemporalType;
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Past;
 
 import java.util.Date;
 
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record CustomerDto(
-        @NotBlank(message = "Firstname should not be empty.")
         String firstName,
-        @NotBlank(message = "Lastname should not be empty.")
         String lastName,
-        @Nullable
         String middleName,
-        @Email
         String email,
         String phoneNumber,
-        @Temporal(TemporalType.DATE)
-        @JsonFormat(pattern = "yyyy-MM-dd")
-        @Past(message = "Birth date should be past date.")
         Date birthDate)
 { }
--- src/main/java/com/test/quotation/model/entity/Customer.java
+++ src/main/java/com/test/quotation/model/entity/Customer.java
@@ -1,6 +1,10 @@
 package com.test.quotation.model.entity;
 
+import jakarta.annotation.Nullable;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -22,20 +26,26 @@ public class Customer {
     Long id;
 
     @Column(name = "firstname")
+    @NotBlank(message = "Firstname should not be empty.")
     private String firstName;
 
     @Column(name = "lastname")
+    @NotBlank(message = "Lastname should not be empty.")
     private String lastName;
 
     @Column(name = "middlename")
+    @Nullable
     private String middleName;
 
     @Column(name = "email")
+    @Email
     private String email;
 
     @Column(name = "phone_number")
     private String phoneNumber;
 
     @Column(name = "birth_date")
+    @Temporal(TemporalType.DATE)
+    @Past(message = "Birth date should be past date.")
     private Date birthDate;
 }
